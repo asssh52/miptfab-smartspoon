d4ba04a2301c348aa3e9b0efda1b41007c30b2a8
Artyom <aptemka9000@gmail.com>
2025-12-10 17:10:53 +0300

    fix

--- fab.cpp
+++ fab.cpp
@@ -7,6 +7,7 @@ const int TILT_PIN = 5;
 HX711 scale;
 
 unsigned long lastUpTime = 0;
+bool          timerStarted = 0;
 volatile bool tiltDetected = false;
 int tiltCount = 0;
 
@@ -15,7 +16,7 @@ void setup() {
 
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
   Serial.println("калибровка тензодатчика...");
-  scale.set_scale(7391);
+  scale.set_scale(4111);
   scale.tare();
 
   pinMode(TILT_PIN, INPUT_PULLUP);
@@ -38,24 +39,39 @@ void loop() {
   if (tiltState == HIGH && !tiltDetected) {
     tiltDetected = true;
     tiltCount++;
-    lastUpTime = millis();
     Serial.print("  Оборотов: ");
     Serial.print(tiltCount);
-  } else if (tiltState == LOW) {
+  }
+  else if (tiltState == LOW) {
     tiltDetected = false;
   }
 
-  if (tiltCount == 10){
-    Serial.println();
-    Serial.print("СБРООООООС через 10 секунд, не трогайте ложку");
-    for (int i = 0; i < 10; i++){
-      Serial.print(".");
-      delay(1000);
-    }
-    tiltCount = 0;
-    scale.tare();
+  if (!tiltState && !timerStarted) {
+    lastUpTime = millis();
+    timerStarted = 1;
+  }
+  else if (tiltState){
+    timerStarted = 0;
   }
 
+  if (!tiltState && timerStarted) {
+    unsigned long now = millis();
+    unsigned long time = now - lastUpTime;
+
+    if (time > 60000) {
+      Serial.println();
+      Serial.print("СБРООООООС через 10 секунд, не трогайте ложку");
+      for (int i = 0; i < 10; i++){
+        Serial.print(".");
+        delay(1000);
+      }
+      tiltCount = 0;
+      scale.tare();
+      timerStarted = 0;
+    }
+  }
+
+  //Serial.print((millis() - lastUpTime) / 1000);
   Serial.println();
   delay(100);
 }
